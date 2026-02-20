cd6b00ebda46eca3d3c7efe0be302f70ef3af9ca
Sina Nanaqdi <nanaqdi48@gmail.com>
2026-02-20 09:10:51 -0600

    create models and superuser

--- CRUD/models.py
+++ CRUD/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class employee(models.Model):
+    fullname = models.CharField(max_length=100 , unique=True)
+    salary = models.PositiveIntegerField(max_length=60000000)
+    hired_date = models.DateField(blank=True, unique=True)     
+
+    def __str__(self):
+        return super().__str__()
+    
