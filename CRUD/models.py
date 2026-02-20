a15c0037fefed06593375dd239eabd168100d54f
Sina Nanaqdi <nanaqdi48@gmail.com>
2026-02-20 11:04:09 -0600

    complete the database

--- CRUD/models.py
+++ CRUD/models.py
@@ -4,10 +4,14 @@ from django.db import models
 
 
 class employee(models.Model):
-    fullname = models.CharField(max_length=100 , unique=True)
+    fullname = models.CharField(max_length=100)
+    role = models.CharField(max_length=50 ,default=None)
     salary = models.PositiveIntegerField(max_length=60000000)
-    hired_date = models.DateField(blank=True, unique=True)     
+    hired_date = models.DateField(blank=True, unique=True)  
+    Email = models.CharField(max_length=60 , unique=True , default=None)   
 
     def __str__(self):
-        return super().__str__()
+        return f"{self.fullname}"
+    
+    
     
